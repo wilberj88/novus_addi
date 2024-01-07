73553b7037001871fc046cd40e164f7a216657da
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-07 17:42:35 +0100

    Update Contable.py

--- pages/Contable.py
+++ pages/Contable.py
@@ -7,3 +7,6 @@ if st.button('Three cheers'):
     st.toast('Hip!')
     time.sleep(.5)
     st.toast('Hooray!', icon='🎉')
+
+
+tipo_empresa = st.selectbox("Selecciona qué tipo de empresa eres:", ("S.L.", "S.A.", "S.L.L.", "Holding", "Comunidades de bienes", "Cooperativas", "Asociaciones", "Autónomos", "Emprendedores", "Particulares"), index=None, placeholder="Choose an option")
