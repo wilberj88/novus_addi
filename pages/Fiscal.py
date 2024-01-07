172c59aa6ae420197fd6ff92798545e795f292b9
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-07 17:47:45 +0100

    Update Fiscal.py

--- pages/Fiscal.py
+++ pages/Fiscal.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import time
 
-if st.button('Three cheers'):
-    st.toast('Hip!')
-    time.sleep(.5)
-    st.toast('Hip!')
-    time.sleep(.5)
-    st.toast('Hooray!', icon='🎉')
+
+tipo_empresa = st.selectbox("Selecciona qué tipo de empresa eres:", ("S.L.", "S.A.", "S.L.L.", "Holding", "Comunidades de bienes", "Cooperativas", "Asociaciones", "Autónomos", "Emprendedores", "Particulares"), index=None, placeholder="Choose an option")
+
+
+servicios_fiscales = st.selectbox("Selecciona qué tipo de servicio fiscal necesitas:", ("Asesoría Tributaria", "Declaraciones Fiscales", "Fiscalidad internacional", "Plan Contable"), index=None, placeholder="Choose an option")
