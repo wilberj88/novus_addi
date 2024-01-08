4b511af0f8239e15259cad3c4d51777046c9633d
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-08 15:25:04 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -10,6 +10,9 @@ import time
 from datetime import time
 import plotly.graph_objects as go
 import pydeck as pdk
+import folium
+from streamlit_folium import st_folium
+from streamlit_extras.grid import grid
 
 
 if "symbols_list" not in st.session_state:
@@ -103,6 +106,123 @@ with params_col:
                         
                     fig1.update_layout(title_text="Usos promedio de tu presupuesto💰", font_size=5)
                     st.plotly_chart(fig1, theme="streamlit")
+
+                                        
+                    my_grid2 = grid(3, vertical_align="bottom")
+                    my_grid2.button("Actualizar Ubicación Clientes", use_container_width=True)
+                    my_grid2.button("Actualizar Ubicación Proveedores", use_container_width=True)
+                    my_grid2.button("Actualizar Ubicación Deudores", use_container_width=True)
+                    
+                    
+                    # center on Liberty Bell, add marker
+                    m = folium.Map(location=[10.4735, -73.2486], zoom_start=13)
+                    n = folium.Map(location=[10.45358, -73.26678], zoom_start=13)
+                    l = folium.Map(location=[10.44664, -73.30750], zoom_start=13)
+                    
+                    #zona1
+                    folium.Marker(
+                        [10.47358, -73.248639], popup="Z1 PV1 PV1 COL Nacional Loperena 3de10K", tooltip="Z1 PV1 PV1 COL Nacional Loperena. 3K de 8K", icon=folium.Icon(icon='cloud')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.474139, -73.25125], popup="Z1 PV2 PV2 ESC Bellas Artes. 2K de 5K", tooltip="Z2 PV2 PV2 ESC Bellas Artes. 2K de 5K", icon=folium.Icon(icon='cloud')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.478472, -73.245361], popup="Z1 PV3 PV3 UDES. 1,5K de 4K", tooltip="Z1 PV3 PV3 UDES. 1,5K de 4K", icon=folium.Icon(icon='cloud')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.468500, -73.247278], popup="Z1 PV4 PV4 COL Prudencia Daza. 1K de 3K", tooltip="Z1 PV4 PV4 COL Prudencia Daza. 1K de 3K", icon=folium.Icon(icon='cloud')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.469667, -73.238056], popup="Z1 PV5 PV5 COL SantoDomingo. 2,5K de 6K", tooltip="Z1 PV5 PV5 COL SantoDomingo. 2,5K de 6K", icon=folium.Icon(icon='cloud')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.474130, -73.251115], popup="PV6 COL Parroquial El Carmelo (Nuevo 2023)", tooltip="PV6 COL Parroquial El Carmelo", icon=folium.Icon(icon='cloud')
+                    ).add_to(m)
+                    #zona2
+                    folium.Marker(
+                        [10.46006, -73.22889], popup="Z2 PV1 PV7 COL Francisco Molina Sanchez. 4K de 9K", tooltip="Z2 PV1 PV7 COL Francisco Molina Sanchez. 4K de 9K", icon=folium.Icon(icon='flag')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.46322, -73.23575], popup="Z2 PV2 PV8 I.E. Manuel Germán Cuello. 2K de 4K", tooltip="Z2 PV1 PV8 I.E. Manuel Germán Cuello. 2K de 4K", icon=folium.Icon(icon='flag')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.45389, -73.24211], popup="Z2 PV3 PV9 Inst. Educ. Leonidas Acuña. 4K de 8K", tooltip="Z2 PV3 PV9 Inst. Educ. Leonidas Acuña. 4K de 8K", icon=folium.Icon(icon='flag')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.45100, -73.23672], popup="Z2 PV4 PV10 UNV. Abierta y a Distancia. 2K de 4,5K", tooltip="Z2 PV4 PV10 UNV. Abierta y a Distancia. 2K de 4,5K", icon=folium.Icon(icon='flag')
+                    ).add_to(m)
+                    #zona3
+                    folium.Marker(
+                        [10.44578, -73.25128], popup="Z3 PV1 PV13 CON. Milciades Cantillo. 3K de 7K", tooltip="Z3 PV1 PV13 CON. Milciades Cantillo. 3K de 7K", icon=folium.Icon(icon='star')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.44950, -73.25075], popup="Z3 PV2 PV14 CON. Alfonso Araujo Cotes. 2K de 5K", tooltip="Z3 PV2 PV14 CON. Alfonso Araujo Cotes. 2K de 5K", icon=folium.Icon(icon='star')
+                    ).add_to(n)
+                    folium.Marker(
+                        [10.45131, -73.25711], popup="Z3 PV3 PV15 INS. TEC. Enrique Pupo. 2K de 5K", tooltip="Z3 PV3 PV15 INS. TEC. Enrique Pupo. 2K de 5K", icon=folium.Icon(icon='star')
+                    ).add_to(n)
+                    folium.Marker(
+                        [10.45714, -73.25153], popup="Z3 PV4 PV16 I.E. Rafael Valle Meza. 2,5K de 6K", tooltip="Z3 PV4 PV16 I.E. Rafael Valle Meza. 2,5K de 6K", icon=folium.Icon(icon='star')
+                    ).add_to(n)
+                    folium.Marker(
+                        [10.43650, -73.25356], popup="Z3 PV5 PV17 I.E. Joaquin Ochoa Mestre. 1,6K de 4K", tooltip="Z3 PV5 PV17 I.E. Joaquin Ochoa Mestre. 1,6K de 4K", icon=folium.Icon(icon='star')
+                    ).add_to(n)
+                    #zona4
+                    folium.Marker(
+                        [10.45358, -73.26678], popup="Z4 PV1 PV20 COL Jose Eugenio Martinez. 5K de 11K", tooltip="Z4 PV1 PV20 COL Jose Eugenio Martinez. 5K de 11K", icon=folium.Icon(icon='info-sign')
+                    ).add_to(n)
+                    folium.Marker(
+                        [10.45892, -73.25958], popup="Z4 PV2 PV21 COL Nacionalizado UPAR. 3,5K de 8K", tooltip="Z4 PV2 PV21 COL Nacionalizado UPAR 8K. 3,5K de 8K", icon=folium.Icon(icon='info-sign')
+                    ).add_to(n)
+                    folium.Marker(
+                        [10.46157, -73.26789], popup="Z4 PV3 PV22 INS TEC Villa Corelca. 1,5K de 4K", tooltip="Z4 PV3 PV22 INS TEC Villa Corelca. 1,5K de 4K", icon=folium.Icon(icon='info-sign')
+                    ).add_to(n)
+                    folium.Marker(
+                        [10.46661, -73.25814], popup="Z4 PV4 PV23 Escuela Mixta No 4. 2K de 5K", tooltip="Z4 PV4 PV23 Escuela Mixta No 4. 2K de 5K", icon=folium.Icon(icon='info-sign')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.44381, -73.27533], popup="Z4 PV5 PV24 I.E Consuelo Araujo Noguera. 2K de 6K", tooltip="Z4 PV5 PV24 I.E Consuelo Araujo Noguera. 2K de 6K", icon=folium.Icon(icon='info-sign')
+                    ).add_to(m)
+                    #zona5
+                    folium.Marker(
+                        [10.47242, -73.26011], popup="Z5 PV1 PV26 COL. Nacionalizado Alfonso López. 2,6K de 7K", tooltip="Z5 PV1 PV26 COL. Nacionalizado Alfonso López. 2,6K de 7K", icon=folium.Icon(icon='bicycle')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.47242, -73.26011], popup="Z5 PV2 PV27 IE Loperena Garupal. 3,2K de 8K", tooltip="Z5 PV2 PV27 IE Loperena Garupal. 3,2K de 8K", icon=folium.Icon(icon='bicycle')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.47928, -73.27719], popup="Z5 PV3 PV28 IE Técnico La Esperanza. 3,7K de 9,3K", tooltip="Z5 PV3 PV28 IE Técnico La Esperanza. 3,7K de 9,3K", icon=folium.Icon(icon='bicycle')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.47842, -73.29089], popup="Z5 PV4 PV29 IE Bello Horizonte. 1,5K de 4K", tooltip="Z5 PV4 PV29 IE Bello Horizonte. 1,5K de 4K", icon=folium.Icon(icon='bicycle')
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.48606, -73.28081], popup="Z5 PV5 PV30 COL COMFACESAR. 2,7K de 7K", tooltip="Z5 PV5 PV30 COL COMFACESAR. 2,7K de 7K", icon=folium.Icon(icon='bicycle')
+                    ).add_to(m)
+                    #zona6
+                    folium.Marker(
+                        [10.48044, -73.24781], popup="Z6 PV1 PV33 COL. Pablo Sexto 5K", tooltip="Z6 PV1 PV33 COL. Pablo Sexto 5K", color='red'
+                    ).add_to(m)
+                    folium.Marker(
+                        [10.48181, -73.25644], popup="Z6 PV2 PV34 CONC.San Joaquin 4K", tooltip="Z6 PV2 PV34 CONC.San Joaquin 4K", color='red'
+                    ).add_to(l)
+                    folium.Marker(
+                        [10.49319, -73.26533], popup="Z6 PV3 PV35 COL. Sagrada Familia 3K", tooltip="Z6 PV3 PV35 COL. Sagrada Familia 3K", color='red'
+                    ).add_to(l)
+                    #zona7
+                    folium.Marker(
+                        [10.47475, -73.25969], popup="Z7 PV1 PV36 INST. TEC. PEDRO CASTRO MONSALVO 8K", tooltip="Z7 PV1 PV36 INST. TEC. PEDRO CASTRO MONSALVO 8K", color='red'
+                    ).add_to(l)
+                    #zona8
+                    folium.Marker(
+                        [10.46442, -73.25625], popup="Z8 PV1 PV37 CARCEL JUDICIAL 0.1K", tooltip="Z8 PV1 PV37 CARCEL JUDICIAL 0.1K", color='red'
+                    ).add_to(l)
+                    folium.Marker(
+                        [10.44664, -73.30750], popup="Z8 PV2 PV38 CARCEL TRAMACUA 0.1K", tooltip="Z8 PV2 PV38 CARCEL TRAMACUA 0.1K", color='red'
+                    ).add_to(l)
+
+
+
                     
                     st.markdown('<p class="dashboard_title">🌎 Diagnóstico Georreferenciado 🔎</p>', unsafe_allow_html = True)
                     
